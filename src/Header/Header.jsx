d742ce99ec291353101cb09c0e316ce869f19cf9
SpanDetectea <SpanDetectea@gmail.com>
2022-09-02 00:37:36 +0300

    update header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -35,8 +35,10 @@ function Header() {
             <Navigation />
         </div>
         <div className="header__search" onBlur={clearInput} tabIndex={1} autoFocus>
-            <input type="text" className='header__search__text' placeholder='Фильмы, сериалы, мультфильмы...' value={search} onInput={(e) => getFilms(e.target.value)} />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Vector_search_icon.svg/111px-Vector_search_icon.svg.png" alt="" className='header__search__lens' />
+            <div className='header__search__input'>
+                <input type="text" className='header__search__input__text' placeholder='Фильмы, сериалы, мультфильмы...' value={search} onInput={(e) => getFilms(e.target.value)} />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Vector_search_icon.svg/111px-Vector_search_icon.svg.png" alt="" className='header__search__input__lens' />
+            </div>
             <div className='header__search__wrapper' >
                 {films && films.map(item => {
                     return <NavLink to={`/film/${item.filmId}`} key={item.filmId}>
@@ -49,7 +51,7 @@ function Header() {
             </div>
         </div>
         <div className="header__signin">
-            <NavLink to = {'/profile'}> Войти</NavLink>
+            <NavLink to={'/profile'}> Войти</NavLink>
             {/* dsad */}
         </div>
     </div>
--- src/Header/Navigation/Navigation.jsx
+++ src/Header/Navigation/Navigation.jsx
@@ -1,19 +1,32 @@
 
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.scss';
 
 function Navigation() {
+    const [active, setActive] = useState(false)
     return <div className='navigation'>
-        <input type="checkbox" id='burger'/>
-        <label htmlFor="burger" className='navigation__burger'></label>
-        <nav className='navigation__wrapper'>
-            <ul>
+        {/* <input type="checkbox" id='burger'/> */}
+        {/* <label htmlFor="burger" className='navigation__burger'></label> */}
+        <div className="navigation__menu" onClick={()=>setActive(!active)}>
+            <span className='navigation__menu__cLine'/>
+        </div>
+        {active ? <nav className='navigation__wrapper'>
+            <ul >
                 <li><Link to='/'>Главная</Link></li>
                 <li><Link to='/movie'>Кино</Link></li>
                 <li><Link to='/series'>Сериалы</Link></li>
                 <li><Link to='/cartoons'>Мультфильмы</Link></li>
             </ul>
-        </nav>
+        </nav> : <nav className='navigation__wrapper navigation__active'>
+            <ul >
+                <li><Link to='/'>Главная</Link></li>
+                <li><Link to='/movie'>Кино</Link></li>
+                <li><Link to='/series'>Сериалы</Link></li>
+                <li><Link to='/cartoons'>Мультфильмы</Link></li>
+            </ul>
+        </nav>}
+        
     </div>
 }
 
